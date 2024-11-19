7044a5b11426ac1665e76569de40dc9c8ae3c036
Cayla3c <00001091951573sp@al.educacao.sp.gov.br>
2024-11-19 20:28:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'RPG',
+    'O que um d20?',
+    'O d20 é um dado de 20 lados'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem descobriu o Brasil?',
+    'Não sei, mas pede pra ele cobrir de novo ai '
+)
+
+criaCartao(
+    'Comida',
+    'O que é uma Melancia?',
+    'Uma Fruta.... sabia dessa!?'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Batat em Inglês?',
+    'Potato.... ué vc n sabia?'
+)
